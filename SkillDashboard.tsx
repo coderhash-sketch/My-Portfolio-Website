584ddabb4d172bb86d59c3f1103ee2454d4ca8e5
Agrim Garg <agrimgrg06@gmail.com>
2026-06-16 15:08:23 +0530

    Update import path for constants in SkillDashboard

--- SkillDashboard.tsx
+++ SkillDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { GAME_SKILLS, GAME_ACHIEVEMENTS } from '../constants';
+import { GAME_SKILLS, GAME_ACHIEVEMENTS } from './constants';
 
 const SkillDashboard: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
