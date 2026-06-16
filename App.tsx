74d3340ed284f09383c800366459a1a9c7120063
Agrim Garg <agrimgrg06@gmail.com>
2026-06-16 16:07:12 +0530

    Remove unused profile picture import

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useMemo } from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Projects from './Projects';
-import profilePicture from './profilePicture.jpeg';
 import Skills from './Skills';
 import Experience from './Experience';
 import Blog from './Blog';
